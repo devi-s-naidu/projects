35fb4b8605aa63bbdb6a4f2a1356b259cfcc9135
devi-s-naidu <devisnaidu2003@gmail.com>
2025-12-23 09:38:29 +0530

    Add files via upload

---
+++ online-shopping-system/body.php
@@ -0,0 +1,629 @@
+ 
+   <div class="main main-raised">
+        <div class="container mainn-raised" style="width:100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+   
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="img/banner3.jpeg" alt="Los Angeles" style="width:100%;">
+        
+      </div>
+
+      <div class="item">
+        <img src="img/banner2.jpeg" style="width:100%;">
+        
+      </div>
+    
+      <div class="item">
+        <img src="img/banner4.jpeg" alt="New York" style="width:100%;">
+        
+      </div>
+      <div class="item">
+        <img src="img/banner1.jpeg" alt="New York" style="width:100%;">
+        
+      </div>
+      <div class="item">
+        <img src="img/banner5.jpeg" alt="New York" style="width:100%;">
+        
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only" >Previous</span>
+    </a>
+    <a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+     
+
+
+		<!-- SECTION -->
+		<div class="section mainn mainn-raised">
+		
+		
+			<!-- container -->
+			<div class="container">
+			
+				<!-- row -->
+				<div class="row">
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=78"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/product53.jpeg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Kwality Walls<br>Oreo</h3>
+								<a href="product.php?p=78" class="cta-btn"></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=72"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/product28.jpeg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>B Natural<br>Mixed Fruits</h3>
+								<a href="product.php?p=72" class="cta-btn"></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=79"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/product08.jpeg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Dark Fantasy<br>Coffee Fills</h3>
+								<a href="product.php?p=79" class="cta-btn"></a>
+							</div>
+                            </div></a>
+					</div>
+					<!-- /shop -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+		  
+		
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+
+					<!-- section title -->
+					<div class="col-md-12">
+						<div class="section-title">
+							<h3 class="title">New Products</h3>
+							<div class="section-nav">
+								<ul class="section-tab-nav tab-nav">
+									<li class="active"><a data-toggle="tab" href="#tab1">Icecreams</a></li>
+									<li><a data-toggle="tab" href="#tab1">Beverages</a></li>
+									<li><a data-toggle="tab" href="#tab1">Condiments</a></li>
+									<li><a data-toggle="tab" href="#tab1">Biscuits</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /section title -->
+
+					<!-- Products tab & slick -->
+					<div class="col-md-12 mainn mainn-raised">
+						<div class="row">
+							<div class="products-tabs">
+								<!-- tab -->
+								<div id="tab1" class="tab-pane active">
+									<div class="products-slick" data-nav="#slick-nav-1" >
+									
+									<?php
+                    include 'db.php';
+								
+                    
+					$product_query = "SELECT * FROM products,categories WHERE product_cat=cat_id AND product_id BETWEEN 10 AND 20";
+                $run_query = mysqli_query($con,$product_query);
+                if(mysqli_num_rows($run_query) > 0){
+
+                    while($row = mysqli_fetch_array($run_query)){
+                        $pro_id    = $row['product_id'];
+                        $pro_cat   = $row['product_cat'];
+                        $pro_brand = $row['product_brand'];
+                        $pro_title = $row['product_title'];
+                        $pro_price = $row['product_price'];
+                        $pro_image = $row['product_image'];
+
+                        $cat_name = $row["cat_title"];
+
+                        echo "
+				
+                        
+                                
+								<div class='product'>
+									<a href='product.php?p=$pro_id'><div class='product-img'>
+										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
+									</div></a>
+									<div class='product-body'>
+										<p class='product-category'>$cat_name</p>
+										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
+										<h4 class='product-price header-cart-item-info '>₹$pro_price</h4>
+										<div class='product-rating'>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+										</div>
+										<div class='product-btns'>
+											<button class='quick-view'><i class='fa fa-eye'></i><span class='tooltipp'>quick view</span></button>
+										</div>
+									</div>
+									<div class='add-to-cart'>
+										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> add to cart</button>
+									</div>
+								</div>
+                               
+							
+                        
+			";
+		}
+        ;
+      
+}
+?>
+										<!-- product -->
+										
+	
+										<!-- /product -->
+										
+										
+										<!-- /product -->
+									</div>
+									<div id="slick-nav-1" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+							</div>
+						</div>
+					</div>
+					<!-- Products tab & slick -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		<!-- HOT DEAL SECTION -->
+		<div id="hot-deal">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<div class="hot-deal">
+							<ul class="hot-deal-countdown">
+								<li>
+									<div>
+										<h3>02</h3>
+										<span>Days</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>10</h3>
+										<span>Hours</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>34</h3>
+										<span>Mins</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>60</h3>
+										<span>Secs</span>
+									</div>
+								</li>
+							</ul>
+							<h2 class="text-uppercase">Hot discounts this week</h2>
+							<p>Get Up to 50% OFF</p>
+							<a class="primary-btn cta-btn" href="store.php">Shop now</a>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /HOT DEAL SECTION -->
+		
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+
+					<!-- section title -->
+					<div class="col-md-12">
+						<div class="section-title">
+							<h3 class="title">Top selling</h3>
+							<div class="section-nav">
+								<ul class="section-tab-nav tab-nav">
+									<li class="active"><a data-toggle="tab" href="#tab2">Condiments</a></li>
+									<li><a data-toggle="tab" href="#tab2">Icecreams</a></li>
+									<li><a data-toggle="tab" href="#tab2">Biscuits</a></li>
+									<li><a data-toggle="tab" href="#tab2">Chips</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /section title -->
+
+					<!-- Products tab & slick -->
+					<div class="col-md-12 mainn mainn-raised">
+						<div class="row">
+							<div class="products-tabs">
+								<!-- tab -->
+								<div id="tab2" class="tab-pane fade in active">
+									<div class="products-slick" data-nav="#slick-nav-2">
+										<!-- product -->
+										<?php
+                    include 'db.php';
+								
+                    
+					$product_query = "SELECT * FROM products,categories WHERE product_cat=cat_id AND product_id BETWEEN 35 AND 44";
+                $run_query = mysqli_query($con,$product_query);
+                if(mysqli_num_rows($run_query) > 0){
+
+                    while($row = mysqli_fetch_array($run_query)){
+                        $pro_id    = $row['product_id'];
+                        $pro_cat   = $row['product_cat'];
+                        $pro_brand = $row['product_brand'];
+                        $pro_title = $row['product_title'];
+                        $pro_price = $row['product_price'];
+                        $pro_image = $row['product_image'];
+
+                        $cat_name = $row["cat_title"];
+
+                        echo "
+				
+                        
+                                
+								<div class='product'>
+									<a href='product.php?p=$pro_id'><div class='product-img'>
+										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
+									</div></a>
+									<div class='product-body'>
+										<p class='product-category'>$cat_name</p>
+										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
+										<h4 class='product-price header-cart-item-info'>₹$pro_price</h4>
+										<div class='product-rating'>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+										</div>
+										<div class='product-btns'>
+											<button class='quick-view'><i class='fa fa-eye'></i><span class='tooltipp'>quick view</span></button>
+										</div>
+									</div>
+									<div class='add-to-cart'>
+										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> add to cart</button>
+									</div>
+								</div>
+                               
+							
+                        
+			";
+		}
+        ;
+      
+}
+?>
+										
+										<!-- /product -->
+									</div>
+									<div id="slick-nav-2" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+							</div>
+						</div>
+					</div>
+					<!-- /Products tab & slick -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-4 col-xs-6">
+						<div class="section-title">
+							<h4 class="title">Top selling</h4>
+							<div class="section-nav">
+								<div id="slick-nav-3" class="products-slick-nav"></div>
+							</div>
+						</div>
+						
+
+						<div class="products-widget-slick" data-nav="#slick-nav-3">
+							<div id="get_product_home">
+								<!-- product widget -->
+								
+								<!-- product widget -->
+							</div>
+
+							<div id="get_product_home2">
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product01.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Chips</p>
+										<h3 class="product-name"><a href="#">Lays sizzling Hot</a></h3>
+										<h4 class="product-price">₹10</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product02.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Chips</p>
+										<h3 class="product-name"><a href="#">Lays India's magic Masala</a></h3>
+										<h4 class="product-price">₹10</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product03.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Chips</p>
+										<h3 class="product-name"><a href="#">Lays crispz Herb & Onion</a></h3>
+										<h4 class="product-price">₹10</h4>
+									</div>
+								</div>
+								<!-- product widget -->
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-4 col-xs-6">
+						<div class="section-title">
+							<h4 class="title">Top selling</h4>
+							<div class="section-nav">
+								<div id="slick-nav-4" class="products-slick-nav"></div>
+							</div>
+						</div>
+
+						<div class="products-widget-slick" data-nav="#slick-nav-4">
+							<div>
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product04.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Condiments</p>
+										<h3 class="product-name"><a href="#">Noodle sticks</a></h3>
+										<h4 class="product-price">₹5</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product05.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Condiments</p>
+										<h3 class="product-name"><a href="#">Maggie</a></h3>
+										<h4 class="product-price">₹10</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product06.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Condiments</p>
+										<h3 class="product-name"><a href="#">Yippee noodles</a></h3>
+										<h4 class="product-price">₹12</h4>
+									</div>
+								</div>
+								<!-- product widget -->
+							</div>
+
+							<div>
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product33.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Biscuits</p>
+										<h3 class="product-name"><a href="#">Unibic</a></h3>
+										<h4 class="product-price">₹10</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product08.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Biscuits</p>
+										<h3 class="product-name"><a href="#">Dark Fantasy</a></h3>
+										<h4 class="product-price">₹40</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product09.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Biscuits</p>
+										<h3 class="product-name"><a href="#">Hide & Seek</a></h3>
+										<h4 class="product-price">₹30</h4>
+									</div>
+								</div>
+								<!-- product widget -->
+							</div>
+						</div>
+					</div>
+
+					<div class="clearfix visible-sm visible-xs">
+					    
+					</div>
+
+					<div class="col-md-4 col-xs-6">
+						<div class="section-title">
+							<h4 class="title">Top selling</h4>
+							<div class="section-nav">
+								<div id="slick-nav-5" class="products-slick-nav"></div>
+							</div>
+						</div>
+
+						<div class="products-widget-slick" data-nav="#slick-nav-5">
+							<div>
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product39.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Icecreams</p>
+										<h3 class="product-name"><a href="#">Amul chocobar Frostik</a></h3>
+										<h4 class="product-price">₹30</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product44.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Icecreams</p>
+										<h3 class="product-name"><a href="#">Amul Icecream Sugrafree</a></h3>
+										<h4 class="product-price">₹40</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product40.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Icecreams</p>
+										<h3 class="product-name"><a href="#">Kwality Walls Hazelnut Chocolate</a></h3>
+										<h4 class="product-price">₹325</h4>
+									</div>
+								</div>
+								<!-- product widget -->
+							</div>
+
+							<div>
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product41.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Chocolates</p>
+										<h3 class="product-name"><a href="#">Cadbury Dairy Milk Silk</a></h3>
+										<h4 class="product-price">₹90</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+								
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product45.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Chocolates</p>
+										<h3 class="product-name"><a href="#">Amul Milk Chocolate</a></h3>
+										<h4 class="product-price">₹100</h4>
+									</div>
+								</div>
+								<!-- /product widget -->
+
+								<!-- product widget -->
+								<div class="product-widget">
+									<div class="product-img">
+										<img src="./img/product42.jpeg" alt="">
+									</div>
+									<div class="product-body">
+										<p class="product-category">Chocolates</p>
+										<h3 class="product-name"><a href="#">Cadbury Bournville</a></h3>
+										<h4 class="product-price">₹94</h4>
+									</div>
+								</div>
+								<!-- product widget -->
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+</div>
+		
